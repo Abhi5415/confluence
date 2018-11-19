c92b18bdd5410b2bd56de6c45755da9f511dc1af
Abhinav Boyed <abhinav.b5415@gmail.com>
2018-11-18 23:33:32 -0500

    chart component

---
+++ client/src/components/scoreChart.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Bar } from 'ant-design-pro/lib/Charts';
+
+const ScoreChart = ({points}) => {
+    const evolutionData = [];
+
+    function addData(points){
+        points.forEach((point) => {
+            evolutionData.push({
+                x: `gen. ${point}`,
+                y: points[point],
+            })
+        })
+    }
+
+    return(
+        <Bar
+            height={200}
+            title="Score Per Generation"
+            data={salesData}
+        />
+    )
+}
+
+export default ScoreChart;
